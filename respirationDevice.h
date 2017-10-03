ffe78382c6c45a0cb4968c667c43dd4abc4ed405
Thomas Deeds <tomdeeds@Thomass-MacBook-Air.local>
2017-10-03 10:19:05 -0400

    updated functions to have seperate sample and read, added RME's

--- respirationDevice.h
+++ respirationDevice.h
@@ -1,21 +1,64 @@
-// returns the respiration status of the patient
-bool respirationDevicRead(RespirationDeviceStruct * this);
+/* collects one sample
+REQUIRES:
+  - Pointer to the struct of this respiration device instance
+  - Properly configured ADC hardware
+MODIFIES:
+  - RespirationDeviceStruct * this
+EFFECTS:
+  - Returns true on error
+*/
+bool respirationDeviceSample(RespirationDeviceStruct * this);
 
-// initializes respiration monitoring hardware
-// returns true on failure
-bool respirationDeviceInit(RespirationDeviceStruct * this);
+/* processes data if necessary and returns (by pointer) the respiration status
+REQUIRES:
+  - Pointer to the struct of this respiration device instance
+MODIFIES:
+  - RespirationDeviceStruct * this
+  - bool * status (breathing or not breathing)
+EFFECTS:
+  - Returns true on error (I.E. not enough readings taken)
+*/
+bool respirationDeviceRead(RespirationDeviceStruct * this, bool * status);
 
-// prepares the respiration monitoring hardware
-// for sleep mode
-// returns true on failure
+/* initializes respiration hardware and software
+REQUIRES:
+  - Pointer to the struct of this respiration device instance
+MODIFIES:
+  - RespirationDeviceStruct * this
+EFFECTS:
+  - Returns true on error (I.E. invalid pins)
+*/
+bool respirationDeviceInit(RespirationDeviceStruct * this, int adcPin, int powerGatePin);
+
+/* turns off respiration monitoring hardware
+REQUIRES:
+  - Pointer to the struct of this respiration device instance
+MODIFIES:
+  - RespirationDeviceStruct * this
+  - Power gates (Hardware)
+  - Pauses signal processing
+EFFECTS:
+  - Returns true on error (I.E. unable to sleep?)
+*/
 bool respirationDeviceSleep(RespirationDeviceStruct * this);
 
-// prepares the respiration monitoring hardware
-// for active mode
-// returns true on failure
+/* Turns on respiration monitoring hardware,
+ * resets signal processing algorithm
+REQUIRES:
+  - Pointer to the struct of this respiration device instance
+MODIFIES:
+  - RespirationDeviceStruct * this
+  - Power gates (Hardware)
+  - Resets signal processing
+EFFECTS:
+  - Returns true on error (I.E. unable to sleep?)
+*/
 bool respirationDeviceActive(RespirationDeviceStruct * this);
 
 typedef struct RespirationDeviceStruct
 {
-    // pins, etc
+    // pins
+    // array of data points
+    // current respiration status
+    // current mode (active, sleep, etc)
 } RespirationDeviceStruct;
--- temperatureDevice.h
+++ temperatureDevice.h
@@ -1,18 +1,55 @@
-// returns the temperature of the patient
-int temperatureDeviceRead(TemperatureDeviceStruct * this);
+/* triggers a read of temperature
+REQUIRES:
+  - Pointer to this temperature device struct
+MODIFIES:
+  - TemperatureDeviceStruct * this
+EFFECTS:
+  - Returns true on error
+*/
+bool temperatureDeviceSample(TemperatureDeviceStruct * this);
 
-// initializes temperature monitoring hardware
-// returns true on failure
+/* returns (by pointer) the temperature
+REQUIRES:
+  - Pointer to this temperature device struct
+MODIFIES:
+  - TemperatureDeviceStruct * this
+  - int * temperature
+EFFECTS:
+  - Returns true on error
+*/
+bool respirationDeviceRead(RespirationDeviceStruct * this, int * temperature);
+
+/* initializes temperature hardware and software
+REQUIRES:
+  - Pointer to this temperature device struct
+MODIFIES:
+  - TemperatureDeviceStruct * this
+EFFECTS:
+  - Returns true on error (I.E. invalid pins)
+*/
 bool temperatureDeviceInit(TemperatureDeviceStruct * this);
 
-// prepares the temperature monitoring hardware
-// for sleep mode
-// returns true on failure
+/* turns off temperature monitoring hardware
+REQUIRES:
+  - Pointer to this temperature device struct
+MODIFIES:
+  - TemperatureDeviceStruct * this
+  - Power gates (Hardware)
+EFFECTS:
+  - Returns true on error (I.E. unable to sleep?)
+*/
 bool temperatureDeviceSleep(TemperatureDeviceStruct * this);
 
-// prepares the temperature monitoring hardware
-// for active mode
-// returns true on failure
+/* Turns on respiration monitoring hardware,
+ * resets signal processing algorithm
+REQUIRES:
+  - Pointer to this temperature device struct
+MODIFIES:
+  - TemperatureDeviceStruct * this
+  - Power gates (Hardware)
+EFFECTS:
+  - Returns true on error (I.E. unable to wake?)
+*/
 bool temperatureDeviceActive(TemperatureDeviceStruct * this);
 
 typedef struct TemperatureDeviceStruct
