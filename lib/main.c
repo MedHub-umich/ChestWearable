8215b3214604af4ee47136c29b773a30925ef9f6
Tyler Siegel <siegelty@umich.edu>
2017-11-03 17:46:32 -0400

    Basic BLE works...?

--- lib/main.c
+++ lib/main.c
@@ -1020,14 +1020,14 @@ static void clock_init(void)
     APP_ERROR_CHECK(err_code);
 }
 
-static void blinky_test(void *pvParameter) {
-    const TickType_t xDelay = 1000.0 / portTICK_PERIOD_MS;
-    while(true) {
-        bsp_board_led_invert(0);
-        bsp_board_led_invert(1);
-        vTaskDelay(xDelay);
-    }
-}
+// static void blinky_test(void *pvParameter) {
+//     const TickType_t xDelay = 1000.0 / portTICK_PERIOD_MS;
+//     while(true) {
+//         bsp_board_led_invert(0);
+//         bsp_board_led_invert(1);
+//         vTaskDelay(xDelay);
+//     }
+// }
 
 
 /**@brief Function for application main entry.
@@ -1071,14 +1071,14 @@ int main(void)
 
     // Create a FreeRTOS task for the BLE stack.
     // The task will run advertising_start() before entering its loop.
-    // nrf_sdh_freertos_init(advertising_start, &erase_bonds);
-    xTaskCreate(blinky_test,
-                "BLINKY",
-                256,
-                NULL,
-                2,
-                NULL
-            );
+    nrf_sdh_freertos_init(advertising_start, &erase_bonds);
+    // xTaskCreate(blinky_test,
+    //             "BLINKY",
+    //             256,
+    //             NULL,
+    //             2,
+    //             NULL
+    //         );
 
     // Start FreeRTOS scheduler.
     vTaskStartScheduler();
