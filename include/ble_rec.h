0610f6f1585f84df43965ef8c7b53a79e325d092
siegelty <siegelty@umich.edu>
2017-11-16 17:33:42 -0500

    Changed to model rec

--- include/ble_rec.h
+++ include/ble_rec.h
@@ -10,7 +10,35 @@ typedef enum {
     BLE_REC_EVT_DIAGNOSTIC
 } ble_rec_evt_type_t;
 
+typedef struct
+{
+    uint8_t const * p_data;           /**< A pointer to the buffer with received data. */
+    uint16_t        length;           /**< Length of received data. */
+} ble_rec_evt_rx_data_t;
+
 // Event structure for rec
-typedef enum {
+typedef struct {
     ble_rec_evt_type_t evt_type;
+    union{
+        ble_rec_evt_rx_data_t rx_data;
+    } params;
 } ble_rec_evt_t;
+
+// Forward delcaration for the rec type
+typedef struct ble_rec_s ble_rec_t;
+
+typedef void (*ble_rec_data_handler_t) (ble_rec_t * p_hrs);
+
+typedef struct {
+    ble_rec_data_handler_t data_handler;
+} ble_rec_init_t;
+
+// Structure for the receive service
+struct ble_rec_s {
+    ble_rec_data_handler_t evt_handler;
+    uint16_t service_handle;
+    uint16_t conn_handle;
+
+};
+
+uint32_t ble_rec_init(ble_rec_t* p_hrs);
