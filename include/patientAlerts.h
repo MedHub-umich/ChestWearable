142afdaca59a68d9290306fabaec88d78e3838ac
deedstho <deedstho@umich.edu>
2017-12-04 23:28:09 -0500

    got the right frequency. ready for master

--- include/patientAlerts.h
+++ include/patientAlerts.h
@@ -6,8 +6,13 @@
 #define LED_ALERT_PIN       7
 #define SPEAKER_ALERT_PIN   27
 
-#define BLINK_DURATION        300
-#define BEEP_DURATION         600
+#define BLINK_DURATION        100
+#define BEEP_DURATION         50
+
+#define PWM_TIMER_FREQUENCY     125000
+#define BEEP_FREQUENCY          2000
+#define BEEP_PERIOD_TICKS       PWM_TIMER_FREQUENCY / BEEP_FREQUENCY
+#define BEEP_DUTY_CYCLE_TICKS   BEEP_PERIOD_TICKS / 2
 
 /*typedef struct PatientAlerts {
     // members
--- lib/patientAlerts.c
+++ lib/patientAlerts.c
@@ -18,142 +18,35 @@ TaskHandle_t  taskAlertSpeakerHandle;
 
 static nrf_drv_pwm_t m_pwm0 = NRF_DRV_PWM_INSTANCE(0);
 
-// This is for tracking PWM instances being used, so we can unintialize only
-// the relevant ones when switching from one demo to another.
-#define USED_PWM(idx) (1UL << idx)
-static uint8_t m_used = 0;
 
-static uint16_t const              m_demo1_top  = 10000;
-static uint16_t const              m_demo1_step = 200;
-static uint8_t                     m_demo1_phase;
-static nrf_pwm_values_individual_t m_demo1_seq_values;
-
-static nrf_pwm_sequence_t const    m_demo1_seq =
+static void initBeep()
 {
-    .values.p_individual = &m_demo1_seq_values,
-    .length              = NRF_PWM_VALUES_LENGTH(m_demo1_seq_values),
-    .repeats             = 0,
-    .end_delay           = 0
-};
-
-static void demo1_handler(nrf_drv_pwm_evt_type_t event_type)
-{
-    if (event_type == NRF_DRV_PWM_EVT_FINISHED)
-    {
-        uint8_t channel    = m_demo1_phase >> 1;
-        bool    down       = m_demo1_phase & 1;
-        bool    next_phase = false;
-
-        uint16_t * p_channels = (uint16_t *)&m_demo1_seq_values;
-        uint16_t value = p_channels[channel];
-        if (down)
-        {
-            value -= m_demo1_step;
-            if (value == 0)
-            {
-                next_phase = true;
-            }
-        }
-        else
-        {
-            value += m_demo1_step;
-            if (value >= m_demo1_top)
-            {
-                next_phase = true;
-            }
-        }
-        p_channels[channel] = value;
-
-        if (next_phase)
-        {
-            if (++m_demo1_phase >= 2 * NRF_PWM_CHANNEL_COUNT)
-            {
-                m_demo1_phase = 0;
-            }
-        }
-    }
-}
-static void demo1(void)
-{
-    NRF_LOG_INFO("Demo 1");
-
-    /*
-     * This demo plays back a sequence with different values for individual
-     * channels (LED 1 - LED 4). Only four values are used (one per channel).
-     * Every time the values are loaded into the compare registers, they are
-     * updated in the provided event handler. The values are updated in such
-     * a way that increase and decrease of the light intensity can be observed
-     * continuously on succeeding channels (one second per channel).
-     */
-
-    nrf_drv_pwm_config_t const config0 =
-    {
-        .output_pins =
-        {
-            27//BSP_LED_0 | NRF_DRV_PWM_PIN_INVERTED, // SPEAKER_BIT_MASK
-        },
-        .irq_priority = APP_IRQ_PRIORITY_LOWEST,
-        .base_clock   = NRF_PWM_CLK_1MHz,
-        .count_mode   = NRF_PWM_MODE_UP,
-        .top_value    = m_demo1_top,
-        .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
-        .step_mode    = NRF_PWM_STEP_AUTO
-    };
-    APP_ERROR_CHECK(nrf_drv_pwm_init(&m_pwm0, &config0, demo1_handler));
-    m_used |= USED_PWM(0);
-
-    m_demo1_seq_values.channel_0 = 0;
-    m_demo1_seq_values.channel_1 = 0;
-    m_demo1_seq_values.channel_2 = 0;
-    m_demo1_seq_values.channel_3 = 0;
-    m_demo1_phase                = 0;
-
-    (void)nrf_drv_pwm_simple_playback(&m_pwm0, &m_demo1_seq, 1,
-                                      NRF_DRV_PWM_FLAG_LOOP);
-}
-
-static void demo5(void)
-{
-    NRF_LOG_INFO("Demo 5");
-
-    /*
-     * This demo, similarly to demo1, plays back a sequence with different
-     * values for individual channels. Unlike demo 1, however, it does not use
-     * an event handler. Therefore, the PWM peripheral does not use interrupts
-     * and the CPU can stay in sleep mode.
-     * The LEDs (1-4) blink separately. They are turned on for 125 ms each,
-     * in counterclockwise order (looking at the board).
-     */
-
     nrf_drv_pwm_config_t const config0 =
     {
         .output_pins =
         {
             SPEAKER_ALERT_PIN
-            //BSP_LED_0 | NRF_DRV_PWM_PIN_INVERTED, // channel 0
-            // BSP_LED_2 | NRF_DRV_PWM_PIN_INVERTED, // channel 1
-            // BSP_LED_3 | NRF_DRV_PWM_PIN_INVERTED, // channel 2
-            // BSP_LED_1 | NRF_DRV_PWM_PIN_INVERTED  // channel 3
         },
         .irq_priority = APP_IRQ_PRIORITY_LOWEST,
         .base_clock   = NRF_PWM_CLK_125kHz,
         .count_mode   = NRF_PWM_MODE_UP,
-        .top_value    = 15625,
+        .top_value    = BEEP_PERIOD_TICKS,
         .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
         .step_mode    = NRF_PWM_STEP_AUTO
     };
     APP_ERROR_CHECK(nrf_drv_pwm_init(&m_pwm0, &config0, NULL));
-    m_used |= USED_PWM(0);
+}
 
-    // This array cannot be allocated on stack (hence "static") and it must
-    // be in RAM (hence no "const", though its content is not changed).
+
+static void startBeep(void)
+{
+    NRF_LOG_INFO("Demo 5");
+
     static nrf_pwm_values_individual_t /*const*/ seq_values[] =
     {
-        { 0x8000,      0,      0,      0 },
-        {      0, 0x8000,      0,      0 },
-        {      0,      0, 0x8000,      0 },
-        {      0,      0,      0, 0x8000 }
+        {BEEP_DUTY_CYCLE_TICKS}
     };
+
     nrf_pwm_sequence_t const seq =
     {
         .values.p_individual = seq_values,
@@ -163,6 +56,12 @@ static void demo5(void)
     };
 
     (void)nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 1, NRF_DRV_PWM_FLAG_LOOP);
+}
+
+
+static void stopBeep()
+{
+    nrf_drv_pwm_stop(&m_pwm0, 0);
 }
 
 
@@ -192,17 +91,13 @@ void taskAlertSpeaker (void * pvParameter)
     {
         waitForNotification(SPEAKER_ALERT_NOTIFICATION);
 
-        //low_power_pwm_start((&low_power_pwm_0), low_power_pwm_0.bit_mask);
-        //APP_ERROR_CHECK(err_code);
-
         NRF_LOG_INFO("Alert.");
 
-        demo5();
+        startBeep();
 
         vTaskDelay(BEEP_DURATION);
-
-        nrf_drv_pwm_stop(&m_pwm0, 0);
-        //low_power_pwm_stop(&low_power_pwm_0);
+        
+        stopBeep();
     }
 }
 
@@ -226,6 +121,8 @@ static void checkReturn(BaseType_t retVal)
 int patientAlertsInit(/*PatientAlerts * this*/)
 {
     uint32_t err_code;
+
+    initBeep();
 
     //Initialize LED for alerts
     nrf_gpio_cfg(
